e8bc1f30411230b6a7ea0c001c0553edf40bf585
Bollinez <bollinez123@gmail.com>
2023-02-23 18:22:03 +0100

    project restructuring

---
+++ clean_cv/js/index.js
@@ -0,0 +1,307 @@
+const origobj = {
+  "summarybanner": {
+    "name": "Sydney Hillen",
+    "image": "../pycv/img/cv_img.jpg",
+    "summary": "A summary of Bolli… (the Bol)"
+  },
+  "personalscontainer": {
+    "address-string": "Obere Belchenstr 17, 5012 Schoenenwerd",
+    "mail": "sydney.hillen@outlook.com",
+    "mobile": "+41 76 565 3714",
+    "linkedin": "/sydney-hillen/"
+  },
+  "workxp-list": [
+    {
+      "department": "(Quantitative Analysis and Technology – Credit Suisse AG, Zurich)",
+      "workxp-title": "Senior Quantitative Analyst",
+      "workxp-time": "02/23-present",
+      "workxp-description": [
+        "development and maintenance of credit risk models using advanced statistical methods, including machine learning models such as gradient boosting or lasso regression in R",
+        "application of standards for code versioning and documentation, as well as issue management using git and Odyssey Bitbucket",
+        "project plan and workflow management using Jira and Kanban",
+        "development of a model prototype using Fsharp and Angular as a proof of concept to move from a waterfall delivery model to a continuous integration and continuous delivery (CI/CD) practice",
+        "automation of the economic downturn analysis for internal-rating-based (IRB) modelling using R Markdown, including: placeholdersubtasks"
+      ],
+      "workxp-description-sub": [
+        "collection, processing and aggregation of macroeconomic data from external sources using R and PL/SQL",
+        "application of regulatory standards for the estimation of economic downturns on portfolio-level"
+      ]
+    },
+    {
+      "department": "(Quantitative Analysis and Technology – Credit Suisse AG, Zurich)",
+      "position": "Senior Model Developer",
+      "workxp-time": "08/20-02/23",
+      "maintasks": [
+        "development and implementation of processes for the monitoring and backtesting of credit risk models using R and Python",
+        "automation of report production for senior management and Swiss Financial Market Supervisory Authority (FINMA) using Python"
+      ],
+      "subtasks": []
+    },
+    {
+      "department": "(Basel Measurement and Reporting – Credit Suisse AG, Zurich)",
+      "position": "Line Management – Regulatory Reporting (IB)",
+      "workxp-time": "10/19-08/20",
+      "maintasks": [
+        "improvement of data quality for Basel III regulatory reporting purposes, by supporting the implementation and testing of change requests from the business side",
+        "reduction of dependencies from multiple web-based BI tools with limited third-party application compatibility and shifting the team's data sourcing towards SQL- and Python-based queries",
+        "review of Compliance- and MIS reports for credit risk- and balance sheet figures for the Swiss Investment Banking business"
+      ],
+      "subtasks": []
+    },
+    {
+      "department": "(Basel Measurement and Reporting – Credit Suisse AG, Zurich)",
+      "position": "Regulatory Reporting (PB)",
+      "workxp-time": "01/17-10/19",
+      "maintasks": [
+        "preparation and presentation of main drivers for credit risk movements to the CFO of Credit Suisse (Switzerland) Ltd.",
+        "performance of test cases and validations for the decommissioning and replacement of applications used for regular business processes",
+        "participation in the working group targeting more frequent reporting cycles as the SME for data flows within PB-related regulatory reporting processes"
+      ],
+      "subtasks": []
+    }
+  ],
+  "student-workxp": [
+    {
+      "department": "Asset Liability Management – Sparkasse KölnBonn, Cologne",
+      "student-workxp-time": "01/15-03/15",
+      "tasks": "Within my internship at Sparkasse KölnBonn I provided assistance to financial engineers, primarily responsible for the management of interest rate risks and asset-/ liability mismatches, but also hat the chance to collaborate with BlackRock representatives for the preparation of the bank's role as a custodian for umbrella funds."
+    },
+    {
+      "department": "Controlling – Bosch Rexroth AG, Witten",
+      "student-workxp-time": "02/14-08/14",
+      "tasks": "My primary responsibilities included the development and improvement of budget planning processes and standardized management control systems."
+    },
+    {
+      "department": "Consulting – amx consulting, Hagen",
+      "student-workxp-time": "01/11-02/11",
+      "tasks": "I conducted research and analyses on candidate profiles for management positions in the fields of search engine optimisation and online media."
+    }
+  ],
+  "education": [
+    {
+      "institution": "FernUniversität Hagen",
+      "area": "Applied Computer Science",
+      "studyType": "Master of Science",
+      "education-time": "2022-2025 (approx.)",
+      "details": {},
+      "topics": [
+        "Architecture and Implementation of Databases",
+        "Parallel Programming",
+        "Data Engineering for Data Science"
+      ]
+    },
+    {
+      "institution": "International School of Management Dortmund",
+      "area": "Finance",
+      "studyType": "Master of Science",
+      "education-time": "09/14-12/16",
+      "details": {
+        "grade": "Excellent with 1.39",
+        "semesterabroad": {
+          "institution": "Oxford Brookes University, Oxford",
+          "grade": "Excellent with 1.0",
+          "education-time": "09/15-12/15"
+        }
+      },
+      "topics": ["Portfolio Modelling (C# based)", "Asset Management"]
+    },
+    {
+      "institution": "International School of Management Dortmund",
+      "area": "International Management",
+      "studyType": "Bachelor of Arts",
+      "education-time": "09/10-08/14",
+      "details": {
+        "grade": "Good with 2.1",
+        "semesterabroad": {
+          "institution": "European Business School, Dublin",
+          "grade": "Excellent with 1.0",
+          "education-time": "01/12-04/12"
+        }
+      },
+      "topics": ["Project Management", "Finance"]
+    }
+  ],
+  "certificates": [
+    {
+      "name": "Financial Risk Manager",
+      "date": "2019",
+      "issuer": "Global Association of Risk Professionals",
+      "image": "../pycv/img/frm_badge.png"
+    },
+    {
+      "name": "Microsoft Certified: Azure Fundamentals",
+      "date": "2023",
+      "issuer": "Microsoft",
+      "image": "../pycv/img/azure_badge.png"
+    }
+  ],
+  "skills": {
+    "verygood": ["PL/SQL", "R", "Markdown", "git", "Python", "VBA", "JSON"],
+    "good": ["FSharp", "SAS", "Anaconda", "Kanban", "HTML", "CSS"],
+    "fundamentals": [
+      "Java",
+      "jQuery",
+      "JavaScript",
+      "Azure",
+      "C#",
+      "Docker",
+      "Angular",
+      "Tableau"
+    ]
+  },
+  "languages": {
+    "native": "German",
+    "fluent": "English",
+    "good": "Spanish"
+  },
+  "interests": {
+    "active": ["my dog", "hiking", "basketball", "shooting range"],
+    "not_active": [
+      "expanding my coding skills",
+      "physical cosmology",
+      "astronomy"
+    ]
+  },
+  "references": [
+    {
+      "name": "Dorothee Bill",
+      "company": "Credit Suisse",
+      "relationship": "Former manager",
+      "title": "Head of IRB Swiss Bank",
+      "department": "Quantitative Analysis and Technology",
+      "contact": "0123456789"
+    },
+    {
+      "name": "Dr. Massimo Cutaia",
+      "company": "Credit Suisse",
+      "relationship": "Hiring manager",
+      "title": "Global Head of Core Credit Modelling",
+      "department": "Quantitative Analysis and Technology",
+      "contact": "0123456789"
+    },
+    {
+      "name": "André Rebmann",
+      "company": "Credit Suisse",
+      "relationship": "Former manager",
+      "title": "Head of Swiss ",
+      "department": "Basel Measurement and Reporting",
+      "contact": "0123456789"
+    }
+  ],
+  "projects_clubs": [
+    "CS Group Finance Innovation Circle (2018-2020)",
+    "CS Valuation in Resolution Data Sourcing (2019)",
+    "CS BMR Innovation Circle & Co-Project lead for Python & R process automation (2018)"
+  ]
+}
+const testobj = {"projects_clubs": [
+  "CS Group Finance Innovation Circle (2018-2020)",
+  "CS Valuation in Resolution Data Sourcing (2019)",
+  "CS BMR Innovation Circle & Co-Project lead for Python & R process automation (2018)"
+]}
+const obj = origobj;
+/*var ch = Object.keys(obj["projects_clubs"][0]);
+var ch = (typeof testobj["projects_clubs"][0] );
+var ch = (typeof obj["references"][0] );
+var ch = Array.isArray(obj["summarybanner"]);
+var ch = (typeof obj["references"][0] );
+var ch = (typeof obj["projects_clubs"][0] );
+
+var datakeys = Object.keys(obj);*/
+//console.log(datakeys[2]);
+
+
+function getDepth(obj){
+  if(!obj || obj.length===0 || typeof(obj)!=="object") return 0;
+  const keys = Object.keys(obj);
+  let depth = 0;
+  keys.forEach(key=>{
+      let tmpDepth = getDepth(obj[key]);
+      if(tmpDepth>depth){
+          depth = tmpDepth;
+      }
+  })
+  return depth+1;
+}
+
+
+function getKeys(obj){
+  var objkeys = Object.keys(obj);
+  return objkeys;
+}
+
+
+
+/*
+var datakeys = Object.keys(testobj);
+let text = "";
+let x = testobj[datakeys[0]];
+let txt = [];
+if(typeof x[0] === "string") {
+x.forEach(myFunction);
+}
+text += txt;
+let check = "demo";
+
+console.log(text);
+function myFunction(value) {
+  txt += "<li>" + value + "</li>";
+} 
+*/
+let listItem = "";
+var datakeys = Object.keys(testobj);
+
+for (var i = 0; i < datakeys.length; i++) {
+  if (Array.isArray(obj[datakeys[i]])) {
+    if(typeof obj[datakeys[i]][0] === "string") {
+      iterateList(obj,datakeys[i]);
+    } else if (typeof obj[datakeys[i]][0] === "object") {
+      var datakeyslower = Object.keys(testobj);
+
+      /*let objarr = obj[datakeys[i]];
+      let listHTML = "";
+      objarr.forEach(createHTMLList);
+      listHTML += listItem;
+      console.log(listHTML);*/
+      //document.getElementById(datakeys[i]).innerHTML = listHTML;
+    } 
+  }
+  // make your new row with the data
+}
+
+function iterateList(object, arrayname) {
+  let objarr = object[arrayname];
+  let listHTML = "";
+  objarr.forEach(createHTMLList);
+  listHTML += listItem;
+  console.log(listHTML);
+ // document.getElementById(arrayname).innerHTML = listHTML;
+}
+
+function createHTMLList(value) {
+  listItem += "<li>" + value + "</li>";
+} 
+/*
+console.log(getDepth(obj.personalscontainer));
+/*
+ var datakeys = Object.keys(obj);
+for (var i = 0; i < datakeys.length; i++) {
+  if (Array.isArray(obj[datakeys[i]])
+    x += (obj[datakeys[i]]
+  // make your new row with the data
+}
+/*
+ var datakeys = Object.keys(obj);
+ var datakeys2 = Object.getOwnPropertyNames(obj);
+ let x = "";
+console.log(datakeys);
+console.log(datakeys2);
+for (var i = 0; i < datakeys.length; i++) {
+  x += obj[datakeys[i]]
+  // make your new row with the data
+}
+console.log(x);
+
+console.log(Object.keys(obj.skills));
+var datakeys = Object.keys(obj.skills);
+console.log(datakeys);  */
