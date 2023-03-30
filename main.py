492975985aef9b6b50d9334791851d3698ef117a
Bollinez <bollinez123@gmail.com>
2023-03-30 20:58:03 +0200

    main comment around format of PDF

--- main.py
+++ main.py
@@ -3,4 +3,4 @@
 # pdf.from_url(r'http://127.0.0.1:3000/pycv/clean_cv/html/cv_hillen.html', 
 #               r'C:\python\pycv\clean_cv\pdf\Hillen_CV2.pdf',
 #               configuration=config)
-#TODO python script to save html as pdf (format not yet the same as saving html manually)
+#TODO python script to save html as pdf (format not yet the same as saving html manually as PDF via print function)
